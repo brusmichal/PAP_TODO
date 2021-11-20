1726562b8b58cd0b76e41afbf6a0c46ada1fef73
Maciek <bukalom98@gmail.com>
2021-11-20 12:33:44 +0100

    Initial project setup

---
+++ src/main/java/com/pap/Pap21zZ25Application.java
@@ -0,0 +1,14 @@
+package com.pap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pap21zZ25Application {
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Pap21zZ25Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/pap/Pap21zZ25ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.pap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pap21zZ25ApplicationTests {
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
