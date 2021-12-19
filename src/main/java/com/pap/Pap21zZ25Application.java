fde57959167b3e104604197906240de078cf85d0
Maciek <bukalom98@gmail.com>
2021-12-19 13:52:59 +0100

    Small sorting refactor

--- src/main/java/com/pap/Pap21zZ25Application.java
+++ src/main/java/com/pap/Pap21zZ25Application.java
@@ -3,7 +3,9 @@ package com.pap;
 import com.pap.view.TaskApplication;
 import javafx.application.Application;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties
 @SpringBootApplication
 public class Pap21zZ25Application {
 
--- src/main/java/com/pap/sort/SortingProperties.java
+++ src/main/java/com/pap/sort/SortingProperties.java
@@ -1,56 +1,41 @@
 package com.pap.sort;
 
-import com.pap.util.yaml.YamlPropertySourceFactory;
 import com.pap.view.task.TaskInstance;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import java.util.Comparator;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @Slf4j
 @Component
-@PropertySource(value = "classpath:sorting/sorting.yml", factory = YamlPropertySourceFactory.class)
+@ConfigurationProperties(prefix = "sorting")
 public class SortingProperties {
 
     @Setter
-    @Value("#{'${sorting.classes}'.split(',')}")
-    private List<String> values;
+    private Map<String, String> classes;
 
     @Getter
     private final Map<String, Comparator<TaskInstance>> comparators = new HashMap<>();
 
+
     @PostConstruct
     void init() throws Exception
     {
-        final var classes = getDescriptionsAndClasses();
-
         // Generate object from class name
         for (final var key : classes.keySet())
         {
-            final var unknownClass = Class.forName(classes.get(key));
+            final var unknownClass = Class.forName(key);
             final var constructor = unknownClass.getConstructor();
             var newComparator = constructor.newInstance();
-            comparators.put(key, (Comparator<TaskInstance>) newComparator);
+            comparators.put(classes.get(key), (Comparator<TaskInstance>) newComparator);
         }
         log.info("Loaded {} taks comparators to use in Done page",comparators.size());
     }
-
-    private Map<String, String> getDescriptionsAndClasses()
-    {
-        final var classes = new HashMap<String,String>();
-        values.forEach(value ->{
-            final var keyValue = value.split(";");
-            classes.put(keyValue[0], keyValue[1]);
-        });
-        return classes;
-    }
-
 }
+
