f20a713e6580669e1fa80933478e1869f3f1e604
Maciek <bukalom98@gmail.com>
2022-01-18 14:33:58 +0100

    Fix custom time period

--- src/main/java/com/pap/view/reports/TimePeriodPanel.java
+++ src/main/java/com/pap/view/reports/TimePeriodPanel.java
@@ -3,6 +3,7 @@ package com.pap.view.reports;
 import com.pap.view.task.buttons.custom.HighlightableButton;
 import com.pap.view.task.buttons.factory.ButtonFactory;
 import com.pap.view.task.buttons.factory.ButtonTypes;
+import javafx.geometry.Pos;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
@@ -22,14 +23,14 @@ public class TimePeriodPanel extends VBox {
     private LocalDateTime customDate;
     private final DatePicker datePicker;
 
-
+    private boolean isCustomClicked = false;
 
     public TimePeriodPanel(final UpdateChart updateChart) {
         this.updateChart = updateChart;
         this.timePeriodOptions = new ArrayList<>();
         this.customDate = LocalDateTime.now();
         this.datePicker = new DatePicker(LocalDate.now());
-
+        this.setAlignment(Pos.CENTER);
         addPeriodButtons();
         addCustomButton();
         setUpPanel();
@@ -43,14 +44,20 @@ public class TimePeriodPanel extends VBox {
         this.getChildren().add(reportBy);
 
         this.setOnMouseEntered((event) -> {
-            this.getChildren().remove(reportBy);
-            this.getChildren().addAll(timePeriodOptions);
-            this.getChildren().add(reportBy);
+            if (!isCustomClicked)
+            {
+                this.getChildren().remove(reportBy);
+                this.getChildren().addAll(timePeriodOptions);
+                this.getChildren().add(reportBy);
+            }
         });
 
         this.setOnMouseExited((event) -> {
-            this.getChildren().removeAll(timePeriodOptions);
-            this.getChildren().remove(datePicker);
+            if(!isCustomClicked)
+            {
+                this.getChildren().removeAll(timePeriodOptions);
+                this.getChildren().remove(datePicker);
+            }
         });
     }
 
@@ -62,9 +69,18 @@ public class TimePeriodPanel extends VBox {
         timePeriods.forEach((key, value) -> {
                     final var button = ButtonFactory.createButtonWithStyle(ButtonTypes.PERIOD);
                     button.setText(key);
-                    button.setOnAction((event) -> updateChart.updateChartArea(value));
+                    button.setOnAction((event) -> {
+                        updateChart.updateChartArea(value);
+                        isCustomClicked = false;
+                        if(getChildren().contains(datePicker))
+                        {
+                            getChildren().remove(datePicker);
+                        }
+                    });
                     button.getStyleClass().add("time-period-button");
                     timePeriodOptions.add(button);
+
+
                 }
         );
     }
@@ -74,14 +90,25 @@ public class TimePeriodPanel extends VBox {
         final var button = ButtonFactory.createButtonWithStyle(ButtonTypes.PERIOD);
         button.setText("Custom...");
         button.setOnAction((event) -> {
-            datePicker.getStyleClass().add("date-selector");
-            if(this.getChildren().contains(reportBy)) {
-                this.getChildren().remove(reportBy);
-                this.getChildren().add(datePicker);
-                this.getChildren().add(reportBy);
+            if(!isCustomClicked)
+            {
+                isCustomClicked = true;
+                datePicker.getStyleClass().add("date-selector");
+                if(this.getChildren().contains(reportBy)) {
+                    this.getChildren().remove(reportBy);
+                    this.getChildren().add(datePicker);
+                    this.getChildren().add(reportBy);
+            }
+            }
+            else {
+                isCustomClicked = false;
+                if(getChildren().contains(datePicker))
+                {
+                    getChildren().remove(datePicker);
+                }
             }
         });
-        datePicker.setOnMouseExited(event -> {
+        datePicker.setOnAction(event -> {
             customDate = datePicker.getValue().atTime(0,0);
             updateChart.updateChartArea(customDate);
         });
